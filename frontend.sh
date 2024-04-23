ee5fcfb65d8a3586d3b7e671ee4083a2a09e6d89
kavya <kavyapothuguntla23@gmail.com>
2024-04-23 16:19:55 -0400

    functions

--- frontend.sh
+++ frontend.sh
@@ -48,7 +48,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/expence-shell/expence.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expence-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
